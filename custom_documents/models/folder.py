c07ba917b7e782c8d82b9ffa0527ebd97b81c6b5
SomagecDji <82517172+SomagecDji@users.noreply.github.com>
2021-10-02 06:21:37 +0100

    Update folder.py

--- custom_documents/models/folder.py
+++ custom_documents/models/folder.py
@@ -153,13 +153,6 @@ class DocumentFolder(models.Model):
                                                                       'condition_type':'domain','domain':'[["mimetype","ilike","pdf"]]',
                                                                      'create_model':'sign.template.direct'})
         return()
-            
-            
-        
-        
-"""class SignSendRequest(models.Model):
-    _description = 'Sign Send Request'
-    _inherit = 'sign.send.request' """
 
 
 
